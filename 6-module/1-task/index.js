d3ae4386de9d331a8e98d1815aca5c5a93ca2e85
evg_dev <e.kabakov@phoenix-cg.ru>
2021-04-28 16:26:11 +0500

    6-module \ 1-task

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,52 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.data = rows;
+    console.log(this.data);
+    this.template = '';
+    this.render(this.data);
+  }
+  onButtonClick(event) {
+    if (!event.target.closest('button')) return;
+    event.target.closest('tr').remove();
+  }
+  render() {
+    if (!this.elem) {
+      this.elem = document.createElement('TABLE');
+      this.template += `
+        <thead>
+          <tr>
+            <th>Имя</th>
+            <th>Возраст</th>
+            <th>Зарплата</th>
+            <th>Город</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+        </tbody>
+      `
+      this.elem.insertAdjacentHTML('beforeEnd', this.template);
+      this.elem.addEventListener('click', event => {
+        this.onButtonClick(event);
+      });
+    }
+    const tableBody = this.elem.querySelector('tbody');
+    let bodyTemplate = '';
+
+    for (const { name, age, salary, city } of this.data) {
+      bodyTemplate += `
+      <tr>
+        <td>${name}</td>
+        <td>${age}</td>
+        <td>${salary}</td>
+        <td>${city}</td>
+        <td><button>X</button></td>
+      </tr>
+      `
+    }
+
+    tableBody.insertAdjacentHTML('beforeEnd', bodyTemplate);
+    return this.elem;
   }
 }
